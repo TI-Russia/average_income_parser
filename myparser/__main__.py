17e3a03c6ece574cccfb1dafe8e9a1a0f85e1364
kbondar17 <ironbondar@gmail.com>
2022-04-04 13:52:06 +0300

    minor changes

--- myparser/__main__.py
+++ myparser/__main__.py
@@ -5,7 +5,7 @@ import traceback
 
 from myparser.parser import Parser
 from myparser.my_logger import get_logger
-from  myparser.config import config
+from myparser.config import config
 from tqdm.auto import tqdm
 
 logger = get_logger(__name__)
@@ -15,9 +15,11 @@ app = typer.Typer()
 
 out_format = config['out_format']
 
+
 @app.command()
 def hello(name: str):
     typer.echo(f"Hello {name}")
+
 
 @app.command()
 def parse_test():
@@ -29,29 +31,28 @@ def parse_test():
 def parse_folder(path):
     cwd = pathlib.Path(__file__).parent.resolve()
     log_file = cwd/'parsing.log'
-    
-    i=0
+
+    i = 0
     bad_files = []
     for file in tqdm(os.listdir(path), position=0, leave=True):
         try:
             parser.parse_file(os.path.join(path, file), out_format=out_format)
-            i+=1
+            i += 1
         except Exception as ex:
             bad_files.append(file)
             logger.error('Ошибка!\n %s', traceback.format_exc())
 
         logger.debug('Сохранили логи в %s', log_file)
-    
-    
-    print('\n\n','='*10, sep='')
+
+    print('\n\n', '='*10, sep='')
     logger.debug('Распаршено документов: %s из %s', i, len(os.listdir(path)))
     print(f'Распаршено документов: {i} из {len(os.listdir(path))} ')
     print('Сохранили в /parsing_results')
     if bad_files:
-        logger.error("Не вышло распарсить:", [e for e in bad_files])
+        logger.error("Не вышло распарсить: %s", [e for e in bad_files])
     print('='*10)
 
-    
+
 @app.command()
 def parse_file(file: str):
     logger.debug('Парсим %s', file)
